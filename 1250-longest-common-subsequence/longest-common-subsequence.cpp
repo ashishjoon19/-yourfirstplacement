1df8a952f3fad00d3ee27480af54cef024cf8db6
Ashish Joon <141510924+ashishjoon19@users.noreply.github.com>
2025-02-04 11:10:31 +0530

    Time: 46 ms (14.22%) | Memory: 27.8 MB (6.37%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,15 +1,14 @@
 class Solution {
     int res,m,n;
-    vector<vector<int>>dp;
-    int find(int i,int j,string &s1,string &s2){
+    int find(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
         if(i >= m || j >= n)
             return 0;
         if(dp[i][j] != -1)
             return dp[i][j];
         if(s1[i] == s2[j])
-            dp[i][j]= 1+find(i+1,j+1,s1,s2);
+            dp[i][j]= 1+find(i+1,j+1,s1,s2,dp);
         else
-            dp[i][j]= max(find(i+1,j,s1,s2),find(i,j+1,s1,s2));
+            dp[i][j]= max(find(i+1,j,s1,s2,dp),find(i,j+1,s1,s2,dp));
         return dp[i][j];
     }
 public:
@@ -17,7 +16,7 @@ public:
         res=0;
         m=text1.length();
         n=text2.length();
-        dp.resize(m+1,vector<int>(n+1,-1));
-        return find(0,0,text1,text2);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return find(0,0,text1,text2,dp);
     }
 };
