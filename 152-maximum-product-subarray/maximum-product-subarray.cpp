15ac8b8a216786fe535787849998fecbc922eeda
Ashish Joon <141510924+ashishjoon19@users.noreply.github.com>
2025-07-25 01:15:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (5.00%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -31,33 +31,9 @@ public:
                 suf[i]=nums[i]*suf[i+1];
         }
         for(int i=0;i<n;i++){
-            cout<<pre[i]<<" "<<suf[i]<<endl;
+            res=max(res,pre[i]);
+            res=max(res,suf[i]);
         }
-        // if(ttl_odd%2==0){
-        //     //even case
-        //     int sum=1;
-        //     for(int i=0;i<n;i++){
-        //         if(nums[i]==0) sum=1;
-        //         else{
-        //             sum*=nums[i];
-        //             res=max(res,sum);
-        //         } 
-        //     }
-        //     return res;
-        // }
-        // else{
-            //odd case
-            for(int i=0;i<n;i++){
-                // if(nums[i]<=0){
-                //     if(i!=0)
-                //         res=max(pre[i-1],res);
-                //     if(i+1<n)
-                //         res=max(res,suf[i+1]);
-                // }
-                res=max(res,pre[i]);
-                res=max(res,suf[i]);
-            }
-            return res;
-        // }
+        return res;
     }
 };
